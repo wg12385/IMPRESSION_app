12c7f80c696998b464e01aa9cecdc07a07c63d86
Will Gerrard <wgerrard@Wills-MBP.home>
2019-12-11 12:10:34 +0000

    update text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          IMPRESSION web app is not running yet, check back soon . . .
         </p>
         <a
           className="App-link"
